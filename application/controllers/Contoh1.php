63930a490646277855f6be8a9e095c5c63966468
RizkiRamadhan30 <rizkiannajah2002@gmail.com>
2023-03-20 12:43:40 +0700

    Menambahkan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class Contoh1 extends CI_Controller {
+    public function index() {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rizki Ramadhan saya tinggal di Petukangan Utara 
+        olahraga yang saya sukai adalah bulutangkis";
+    }
+}
